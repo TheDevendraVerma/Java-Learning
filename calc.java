329e51c0727ae4094c7c62c1fab81e3784f52462
Devendra Verma <Devv70171@gamil.com>
2026-05-23 16:36:04 +0530

    starting java programing

---
+++ calc.java
@@ -0,0 +1,15 @@
+ class calc {
+    public static void main(String arg[]) {
+ int a=15 ;
+ int b =25; 
+ int c=30 ;
+int sum = 0 ; 
+sum = a+b;
+int mult = 0 ;
+mult = a *b*c ;
+System.out.println(+sum) ;
+System.out.println(+mult) ;
+System.out.println("hello this calculator created by dev") ;
+ 
+}
+}
