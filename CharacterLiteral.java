4c55485e5576c730d1d7a8cf3a87b1f6758d3be8
Devendra Verma <Devv70171@gamil.com>
2026-06-08 15:06:41 +0530

    codes

---
+++ CharacterLiteral.java
@@ -0,0 +1,11 @@
+public class CharacterLiteral {
+    public static void main(String args[]){
+        char a='A'; // Character literal
+        char b=65; // ASCII value for 'A'
+        char c='\u0041'; // Unicode representation for 'A'
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c); 
+    }
+}
---
+++ Devendra.java
@@ -0,0 +1,8 @@
+public class Devendra{
+    public static void main(String args[]){
+       System.out.println("Hello DEVENDRA Bhai");
+       int a=10,b=20;
+       int c=a+b;
+       System.out.println("Sum is "+c);
+      }
+} 
